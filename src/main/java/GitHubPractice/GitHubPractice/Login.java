8b3ba144b3fd2da5edfe459f93d1450882b36c1f
hiteshdarji162021 <hiteshdarji162021@gmail.com>
2024-01-02 14:43:51 +0530

    first commit

---
+++ src/main/java/GitHubPractice/GitHubPractice/Login.java
@@ -0,0 +1,11 @@
+package GitHubPractice.GitHubPractice;
+
+public class Login {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hitesh Login");
+
+	}
+
+}
---
+++ src/main/java/GitHubPractice/GitHubPractice/Registration.java
@@ -0,0 +1,11 @@
+package GitHubPractice.GitHubPractice;
+
+public class Registration {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hitesh Registration");
+
+	}
+
+}
